2f6b9f433625863d377cd083c24d53e6c995f40b
Harshad Raj Gupta <gptharshad@gmail.com>
2026-03-13 18:23:50 +0530

    update app.py for render host

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import requests
+import os
+
 
 app = Flask(__name__)
 
@@ -69,4 +71,11 @@ def index():
     )
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
+
+
+
+
